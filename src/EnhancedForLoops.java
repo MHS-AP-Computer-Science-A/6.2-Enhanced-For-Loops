2fc21534cb0a68f9ea922ef440187790f7d53623
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-04-27 14:15:41 -0400

    Add files via upload

--- src/EnhancedForLoops.java
+++ src/EnhancedForLoops.java
@@ -1,36 +1,38 @@
 
 public class EnhancedForLoops {
 	public static void main(String[] args) {
-		//Create an array with an initializer list
-		int[] arr = {1, 2, 3, 4};
-				
-		//Print the array with a traditional for-loop
-		for(int i = 0; i < arr.length; i++){
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println();
+		// Create an array with an initializer list
+		int[] arr = { 1, 2, 3, 4 };
 
-		//Print the array with a for-each loop
-		for(int item : arr){
+		// Print the array with a for-each loop
+		for (int item : arr) {
 			System.out.print(item + " ");
 		}
 		System.out.println();
-		
-		
-		//Create an array by specifying the size
-		//and fill an array by accessing indexes
-		String[] words = new String[4];  
-		words[0] = "Hello";
-		words[1] = "Goodbye";
-		words[2] = "Avocado";
-		words[3] = "Taco";
 
-		//Display words longer than 5 letters
-		for(String w : words) {
-			if(w.length() > 5) {
-				System.out.println(w);
+		// Create an array with an initializer list
+		String[] words = { "cat", "sat", "splat" };
+
+		// Print the words that start with s
+		for (String s : words) {
+			if (s.charAt(0) == 's') {
+				System.out.print(s + " ");
 			}
 		}
+
+		System.out.println();
+
+		Point[] points = { new Point(1, 2), new Point(3, 4), new Point(4, 5) };
+
+		// Display words longer than 5 letters
+		for (Point p : points) {
+			p.x++;
+			p = new Point(0, 0);
+		}
+
+		for (Point p : points) {
+			System.out.println("(" + p.x + ", " + p.y + ")");
+		}
 	}
-		
+
 }
